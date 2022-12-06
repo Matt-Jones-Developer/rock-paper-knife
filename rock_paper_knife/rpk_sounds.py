c7da0ba6ad4c9a733800653eecac3bb6cba4d5db
Matt Jones <glitchyghost81@protonmail.com>
2022-12-06 08:42:29 +0000

    added audio files and updated the paths

--- rock_paper_knife/rpk_sounds.py
+++ rock_paper_knife/rpk_sounds.py
@@ -10,7 +10,7 @@ def coin_up():
     c.play()
     c.set_volume(0.70)
 try:
-    c = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/smw_coin.wav")
+    c = mixer.Sound("assets/audio/smb_coin.wav")
 except:
     prompt = "Error: Sound file not found"
 # startup sound
@@ -18,7 +18,7 @@ def presents_up():
     pr.play()
     pr.set_volume(0.70)
 try:
-    pr = mixer.Sound("/Users/glitchy81/Python/assets/wav/voice/presenting.wav")
+    pr = mixer.Sound("assets/audio/presenting.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -27,7 +27,7 @@ def glitchy_up():
     g.play()
     g.set_volume(0.70)
 try:
-    g = mixer.Sound("/Users/glitchy81/Python/assets/wav/voice/uh_hello_fast.wav")
+    g = mixer.Sound("assets/audio/uh_hello_fast.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -36,7 +36,7 @@ def rock_up():
     rock.play()
     rock.set_volume(0.60)
 try:
-    rock = mixer.Sound("/Users/glitchy81/Python/assets/wav/voice/rock.wav")
+    rock = mixer.Sound("assets/audio/rock.wav")
 except:
     prompt = "Error: Sound file not found"
 # PAPER sound
@@ -44,7 +44,7 @@ def paper_up():
     pap.play()
     pap.set_volume(0.50)
 try:
-    pap = mixer.Sound("/Users/glitchy81/Python/assets/wav/voice/paper.wav")
+    pap = mixer.Sound("assets/audio/paper.wav")
 except:
     prompt = "Error: Sound file not found"
 # KNIFE sound
@@ -52,7 +52,7 @@ def knife_up():
     kn.play()
     kn.set_volume(0.50)
 try:
-    kn = mixer.Sound("/Users/glitchy81/Python/assets/wav/voice/knife.wav")
+    kn = mixer.Sound("assets/audio/knife.wav")
 except:
     prompt = "Error: Sound file not found"
 # KNIFE sound
@@ -60,7 +60,7 @@ def chug_up():
     chug.play()
     chug.set_volume(0.30)
 try:
-    chug = mixer.Sound("/Users/glitchy81/Python/assets/wav/guitar/chug_one.wav")
+    chug = mixer.Sound("assets/audio/chug_one.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -69,7 +69,7 @@ def power_up():
     p.play()
     p.set_volume(0.80)
 try:
-    p = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/nsmb_power-up.wav")
+    p = mixer.Sound("assets/audio/nsmb_power-up.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -78,7 +78,7 @@ def cpu_up():
     cpu.play()
     #cpu.set_volume(0.90)
 try:
-    cpu = mixer.Sound("/Users/glitchy81/Python/assets/wav/voice/computing.wav")
+    cpu = mixer.Sound("assets/audio/computing.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -86,7 +86,7 @@ def shell_up():
     sh.play()
     sh.set_volume(0.90)
 try:
-    sh = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/smw_pipe.wav")
+    sh = mixer.Sound("assets/audio/smw_pipe.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -95,7 +95,7 @@ def player_up():
     pl.play()
     pl.set_volume(0.70)
 try:
-    pl = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/smw_pause.wav")
+    pl = mixer.Sound("assets/audio/smw_pause.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -103,7 +103,7 @@ def bump_up():
     b.play()
     b.set_volume(0.70)
 try:
-    b = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/smb_bump.wav")
+    b = mixer.Sound("assets/audio/smb_bump.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -112,7 +112,7 @@ def ready_up():
     r.play()
     r.set_volume(0.70)
 try:
-    r = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/smw_princess_help.wav")
+    r = mixer.Sound("assets/audio/smw_princess_help.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -121,7 +121,7 @@ def battle_up():
     ba.play()
     ba.set_volume(0.40)
 try:
-    ba = mixer.Sound("/Users/glitchy81/Python/assets/wav/guitar/battle.wav")
+    ba = mixer.Sound("assets/audio/battle.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -130,7 +130,7 @@ def countdown():
     co.play()
     co.set_volume(0.70)
 try:
-    co = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/kart_timer.wav")
+    co = mixer.Sound("assets/audio/kart_timer.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -139,7 +139,7 @@ def ktimer3():
     k3.play()
     k3.set_volume(0.70)
 try:
-    k3 = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/kart_timer3.wav")
+    k3 = mixer.Sound("assets/audio/kart_timer3.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -147,7 +147,7 @@ def ktimer2():
     k2.play()
     k2.set_volume(0.70)
 try:
-    k2 = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/kart_timer2.wav")
+    k2 = mixer.Sound("assets/audio/kart_timer2.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -155,7 +155,7 @@ def ktimer1():
     k1.play()
     k1.set_volume(0.70)
 try:
-    k1 = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/kart_timer1.wav")
+    k1 = mixer.Sound("assets/audio/kart_timer1.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -163,7 +163,7 @@ def one_up():
     up.play()
     up.set_volume(0.70)
 try:
-    up = mixer.Sound("/Users/glitchy81/Python/assets/wav/smb_faves/smw_1-up.wav")
+    up = mixer.Sound("assets/audio/smw_1-up.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -171,7 +171,7 @@ def win_up():
     wi.play()
     wi.set_volume(0.70)
 try:
-    wi = mixer.Sound("/Users/glitchy81/Python/assets/app_game_audio/Super_Mario_Archive/Random_mario/sm64_bowser_message.wav")
+    wi = mixer.Sound("assets/audio/sm64_bowser_message.wav")
 except:
     prompt = "Error: Sound file not found"
 # nsmb power up sound
@@ -179,7 +179,7 @@ def lose_up():
     lo.play()
     lo.set_volume(0.70)
 try:
-    lo = mixer.Sound("/Users/glitchy81/Python/assets/app_game_audio/Super_Mario_Archive/Random_mario/sm64_bowser_message.wav")
+    lo = mixer.Sound("assets/audio/sm64_bowser_message.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -188,7 +188,7 @@ def thanks_up():
     th.play()
     th.set_volume(0.70)
 try:
-    th = mixer.Sound("/Users/glitchy81/Python/assets/app_game_audio/Super_Mario_Archive/Random_mario/sm64_mario_thank_you.wav")
+    th = mixer.Sound("assets/audio/sm64_mario_thank_you.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -197,7 +197,7 @@ def insert_coin():
     insert.play()
     insert.set_volume(0.50)
 try:
-    insert = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/coin_insert.wav")
+    insert = mixer.Sound("assets/audio/coin_insert.wav")
 except:
     prompt = "Error: Sound file not found"
 # MK2 sounds
@@ -205,7 +205,7 @@ def fight_up():
     f.play()
     f.set_volume(0.30)
 try:
-    f = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/mk_fight.wav")
+    f = mixer.Sound("assets/audio/mk_fight.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -213,7 +213,7 @@ def kom_dun():
     ko.play()
     ko.set_volume(0.50)
 try:
-    ko = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/kombat_dun.wav")
+    ko = mixer.Sound("assets/audio/kombat_dun.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -222,7 +222,7 @@ def error_up():
     er.play()
     er.set_volume(0.50)
 try:
-    er = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/mk2_error.wav")
+    er = mixer.Sound("assets/audio/mk2_error.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -231,7 +231,7 @@ def gong_up():
     go.play()
     go.set_volume(0.50)
 try:
-    go = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/mk2_gong.wav")
+    go = mixer.Sound("assets/audio/mk2_gong.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -239,7 +239,7 @@ def life_up():
     li.play()
     li.set_volume(0.70)
 try:
-    li = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/SF_life.wav")
+    li = mixer.Sound("assets/audio/SF_life.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -247,7 +247,7 @@ def peak_up():
     pe.play()
     pe.set_volume(0.70)
 try:
-    pe = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/SF_peak.wav")
+    pe = mixer.Sound("assets/audio/SF_peak.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -255,7 +255,7 @@ def punch_up():
     pu.play()
     pu.set_volume(0.70)
 try:
-    pu = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/mk2_punch1.wav")
+    pu = mixer.Sound("assets/audio/mk2_punch1.wav")
 except:
     prompt = "Error: Sound file not found"
 
@@ -263,6 +263,6 @@ def punch2_up():
     p2.play()
     p2.set_volume(0.70)
 try:
-    p2 = mixer.Sound("/Users/glitchy81/Python/assets/wav/fighter/mk2_punch2.wav")
+    p2 = mixer.Sound("assets/audio/mk2_punch2.wav")
 except:
     prompt = "Error: Sound file not found"
